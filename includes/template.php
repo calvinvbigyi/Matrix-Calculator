73c023d56d9aedb1226c440034e055fcdf68b9cd
calvinvbigyi <vbigyi@gmail.com>
2014-09-02 01:06:25 -0400

    add some radio buttons

--- includes/template.php
+++ includes/template.php
@@ -16,6 +16,9 @@
             <div id="content_area">
                   <form method="post" action="index.php">
                     <textarea id="result_matrix" name="result_matrix"></textarea><br></br>
+                    <input type="radio" name="matrix_f" value="det"> Calculate the Determinant of the matrix <br>
+                    <input type="radio" name="matrix_f" value="add"> Calculate the sum of two matrixes<br>
+                    <input type="radio" name="matrix_f" value="sub"> Calculate one matrix subtracts another matrix<br></br>
                     <input type="submit" name="submit" value="Calculate Now!" id="submit"/>
                   </form>
                 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>                
